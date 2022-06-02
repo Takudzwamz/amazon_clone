ab6c6dd7a076089e0e2a1519cf3fcc54c2b01ab1
Takudzwamz <zhout1709@hotmail.com>
2022-06-02 16:57:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+            ),
+          ),
+      ),
+      home:  Scaffold(
+        appBar: AppBar(
+          title:  const Text('Amazon clone'),
+        ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Flutter Demo Home Page',
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text('Click me')),
+          ],
+        ),
+        ),
+    );
+  }
+}
